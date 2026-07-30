b2699675abe5b76567ac9b9ea2a520c1f3c11022
moustapha <moustafdrame95@gmail.com>
2026-07-30 12:51:42 +0000

    supprime deuxieme projet$

--- mon_project/monsite/capteur/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CapteurConfig(AppConfig):
-    name = 'capteur'
--- mon_project/monsite/capteurs/views.py
+++
@@ -1,7 +0,0 @@
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def accueil(request): 
-    return HttpResponse("<p>Bienvenue sur ma page capteurs<p\>")
